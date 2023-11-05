098bbf8a2b141cee5ed8bf2b6797dbc8bd600f85
talibnabi <talibnabiyev@gmail.com>
2023-11-05 06:47:31 -0500

    nothing special

--- taskify/src/main/java/org/taskifyapp/util/JavaMailSenderConstants.java
+++ taskify/src/main/java/org/taskifyapp/util/JavaMailSenderConstants.java
@@ -10,7 +10,6 @@ public class JavaMailSenderConstants {
             + "[[CODE]]<br>"
             + "Thank you,<br>"
             + "Good Luck, taskify corporation.";
-
     public static final String SEND_TO_USER_CONTENT = "Hi,user [[USER_KEY]],<br>"
             + "You have been registered by admin.<br>"
             + "Your admin is: [[ADMIN_KEY]].<br>"
@@ -19,19 +18,12 @@ public class JavaMailSenderConstants {
             + "Thank you,<br>";
     public static final String SEND_REGISTRATION_SUBJECT = "Verify your registration";
     public static final String SEND_TO_USER_SUBJECT = "Welcome!";
-
     public static final String NAME = "[[NAME]]";
-
     public static final String CODE = "[[CODE]]";
-
     public static final String USER_KEY = "[[USER_KEY]]";
-
     public static final String ADMIN_KEY = "[[ADMIN_KEY]]";
-
     public static final String ORGANIZATION_KEY = "[[ORGANIZATION_KEY]]";
-
     public static final Integer SENDING_CODE_LENGTH = 7;
-
     public static final String SENDER_ADDRESS = "";
     public static final String SENDER_NAME = "";
 
